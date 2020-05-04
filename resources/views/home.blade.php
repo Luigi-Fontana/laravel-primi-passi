3254c67b85d4c481ac96978ac1a85bcf6d80375f
Luigi Fontana <luigifontana92@gmail.com>
2020-05-04 16:55:16 +0200

    Prova web.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>La molinara</title>
+    </head>
+    <body>
+        <h1>La molinara</h1>
+    </body>
+</html>
