0db77bc100df73961f5ad6ffaaae271ef060a5f6
Luigi Fontana <luigifontana92@gmail.com>
2020-05-07 17:09:29 +0200

    Svolgimento bonus: frecce di scorrimento tra prodotti

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -1,6 +1,8 @@
 @php
     $prodotti = config('prodotti');
     $prodotto = $prodotti[$id];
+    $left = $id - 1;
+    $right = $id + 1;
 @endphp
 @extends('layouts.layout')
 @section('titolo')
@@ -16,6 +18,16 @@
                     <img src="{{$prodotto['src-p']}}" alt="{{$prodotto['titolo']}}">
                     <h3>{!!$prodotto['descrizione']!!}</h3>
                     <h4><a href="{{route('prodotti')}}">Torna ai prodotti</a></h4>
+                    @if ($id != 0)
+                        <div class="left">
+                            <a href="{{route('prodotti.show', $left)}}">&#10094;</a>
+                        </div>
+                    @endif
+                    @if ($id != 11)
+                        <div class="right">
+                            <a href="{{route('prodotti.show', $right)}}">&#10095;</a>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
