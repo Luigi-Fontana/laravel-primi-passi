5b2e47da851049fbc8ababc31a4f8e20a1caa468
Luigi Fontana <luigifontana92@gmail.com>
2020-05-08 10:00:49 +0200

    Bugfix

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -3,6 +3,7 @@
     $prodotto = $prodotti[$id];
     $left = $id - 1;
     $right = $id + 1;
+    $length = count($prodotti);
 @endphp
 @extends('layouts.layout')
 @section('titolo')
@@ -12,23 +13,25 @@
     <main>
         <div class="bg-product">
             <div class="container">
-                <div class="product">
-                    <h2>{{$prodotto['titolo']}}</h2>
-                    <img src="{{$prodotto['src-h']}}" alt="{{$prodotto['titolo']}}">
-                    <img src="{{$prodotto['src-p']}}" alt="{{$prodotto['titolo']}}">
-                    <h3>{!!$prodotto['descrizione']!!}</h3>
-                    <h4><a href="{{route('prodotti')}}">Torna ai prodotti</a></h4>
-                    @if ($id != 0)
-                        <div class="left">
-                            <a href="{{route('prodotti.show', $left)}}">&#10094;</a>
-                        </div>
-                    @endif
-                    @if ($id != 11)
-                        <div class="right">
-                            <a href="{{route('prodotti.show', $right)}}">&#10095;</a>
-                        </div>
-                    @endif
-                </div>
+                @if (!empty($prodotto))
+                    <div class="product">
+                        <h2>{{$prodotto['titolo']}}</h2>
+                        <img src="{{$prodotto['src-h']}}" alt="{{$prodotto['titolo']}}">
+                        <img src="{{$prodotto['src-p']}}" alt="{{$prodotto['titolo']}}">
+                        <h3>{!!$prodotto['descrizione']!!}</h3>
+                        <h4><a href="{{route('prodotti')}}">Torna ai prodotti</a></h4>
+                        @if ($id > 0)
+                            <div class="left">
+                                <a href="{{route('prodotti.show', $left)}}">&#10094;</a>
+                            </div>
+                        @endif
+                        @if ($id < $length - 1)
+                            <div class="right">
+                                <a href="{{route('prodotti.show', $right)}}">&#10095;</a>
+                            </div>
+                        @endif
+                    </div>
+                @endif
             </div>
         </div>
     </main>
